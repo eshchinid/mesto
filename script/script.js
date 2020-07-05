03a5e63821983074a0357440950102b6e69506c6
Ivan Eshchin <eshchin.id@gmail.com>
2020-07-06 00:17:12 +0300

    первый комит на проверку

---
+++ script/script.js
@@ -0,0 +1,34 @@
+const openModalButton = document.querySelector(".profile__btn-edit");
+const editModalWindow = document.querySelector(".modal");
+const closeModalButton = document.querySelector(".modal__close");
+const profileName = document.querySelector('.profile__name');
+const profileRole = document.querySelector('.profile__role');
+const editModalSave = document.querySelector('.modal__save');
+const editProfileName = document.querySelector('.modal__input_name');
+const editProfileRole = document.querySelector('.modal__input_role');
+
+
+function toggleModal() {
+    editModalWindow.classList.toggle("modal__opened");
+}
+
+openModalButton.addEventListener("click", toggleModal);
+closeModalButton.addEventListener("click", toggleModal);
+
+function defaultText() {
+    profileName.value = editProfileName.textContent;
+    profileRole.value = editProfileRole.textContent;
+}
+
+function saveInfo(event) {
+    event.preventDefault();
+    profileName.textContent = editProfileName.value;
+    profileRole.textContent = editProfileRole.value;
+    toggleModal()
+}
+
+openModalButton.addEventListener('click', defaultText);
+editModalSave.addEventListener('click', saveInfo);
+
+
+
