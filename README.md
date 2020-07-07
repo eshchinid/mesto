c54cfa7f3f8462f35a185689178a23ee1ce5fd0a
Ivan Eshchin <eshchin.id@gmail.com>
2020-07-07 21:54:37 +0300

    правки внесены

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
 * [Ссылка на макет в Figma](https://www.figma.com/file/StZjf8HnoeLdiXS7dYrLAh/JavaScript.-Sprint-4)
 
 проект про БЭМ, чтоб его
+
+https://eshchinid.github.io/mesto/
--- script/script.js
+++ script/script.js
@@ -3,10 +3,9 @@ const editModalWindow = document.querySelector(".modal");
 const closeModalButton = document.querySelector(".modal__close");
 const profileName = document.querySelector('.profile__name');
 const profileRole = document.querySelector('.profile__role');
-const editModalSave = document.querySelector('.modal__save');
 const editProfileName = document.querySelector('.modal__input_name');
 const editProfileRole = document.querySelector('.modal__input_role');
-const editSaveForm = document.querySelector('.modal__content');
+const modalSave = document.querySelector('.modal__form');
 
 
 function toggleModal() {
@@ -20,20 +19,15 @@ function saveInfo(event) {
     toggleModal()
 }
 
+function openModal() {
+    toggleModal();
+    editProfileName.value = profileName.textContent;
+    editProfileRole.value = profileRole.textContent;
+}
 
-openModalButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    profileName.textContent = editProfileName.value;
-    profileRole.textContent = editProfileRole.value;
-    toggleModal()
-});
-
-closeModalButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    toggleModal()
-});
-editModalSave.addEventListener('click', saveInfo);
-
+openModalButton.addEventListener("click", openModal);
+closeModalButton.addEventListener("click", toggleModal);
+modalSave.addEventListener('submit', saveInfo);
 
 
 
